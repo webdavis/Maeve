771aa79c89402b720475b9c05dddf1fca8c64dba
Stephen A. Davis <stephen@webdavis.io>
2024-12-21 19:12:50 -0700

    Create `LocationData` module

---
+++ Maeve/LocationKit/LocationData.swift
@@ -0,0 +1,14 @@
+//
+//  LocationData.swift
+//  LocationKit
+//
+//  Created by Stephen Davis on 2024-12-21.
+//
+
+import Foundation
+
+struct LocationData {
+  let id: UUID
+  let latitude: String
+  let longitude: String
+}
