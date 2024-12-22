a7eb0a083fe186328fa5e4783b825e65614b3bc0
Stephen A. Davis <stephen@webdavis.io>
2024-12-21 19:10:49 -0700

    Create `LocationKit` framework

---
+++ Maeve/LocationKit/LocationKit.h
@@ -0,0 +1,18 @@
+//
+//  LocationKit.h
+//  LocationKit
+//
+//  Created by Stephen Davis on 2024-12-21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocationKit.
+FOUNDATION_EXPORT double LocationKitVersionNumber;
+
+//! Project version string for LocationKit.
+FOUNDATION_EXPORT const unsigned char LocationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationKit/PublicHeader.h>
+
+
---
+++ Maeve/LocationKitTests/LocationKitTests.swift
@@ -0,0 +1,20 @@
+//
+//  LocationKitTests.swift
+//  LocationKitTests
+//
+//  Created by Stephen Davis on 2024-12-21.
+//
+
+import XCTest
+@testable import LocationKit
+
+final class LocationKitTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+}
